9a5e23e59ebe4b616911f8b0002446d2ce791cc0
0herm <0herm@protonmail.com>
2025-11-06 15:37:07 +0100

    fix navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import 'uibee/styles'
 import './globals.css'
 import { cookies } from 'next/headers'
-import TopBar from '@components/navbar/topbar'
+import Navbar from '@components/navbar/navbar'
 
 export const metadata: Metadata = {
     title: 'BeeFormed',
@@ -15,15 +15,13 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
     
     return (
         <html lang='en' className={`${theme} h-full`}>
-            <body className='bg-login-700 h-full flex flex-col'>
-                <div className="flex flex-col min-h-screen">
-                    <header className='w-full bg-[#1e1e1e99] backdrop-blur-md'>
-                        <TopBar />
-                    </header>
-                    <main className='flex-1 flex overflow-hidden'>
-                        <div className='w-full bg-login-800'>{children}</div>
-                    </main>
-                </div>
+            <body className='bg-login-800 h-full flex flex-col'>
+                <header className='w-full bg-[#1e1e1e99] backdrop-blur-md h-fit'>
+                    <Navbar />
+                </header>
+                <main className='flex-1 flex overflow-hidden pt-22 w-full bg-login-800'>
+                    {children}
+                </main>
             </body>
         </html>
     )
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { getCookie } from 'uibee/utils'
+import { Navbar as NavBar, NavItem } from 'uibee/components'
+import config from '@config'
+
+export default function Navbar() {
+    const [token, setToken] = useState<string | null>(null)
+
+    useEffect(() => {
+        setToken(getCookie('access_token'))
+    }, [])
+
+    return (
+            <NavBar
+                token={token}
+                disableLanguageToggle={true}
+                lang={undefined}
+                theme={'dark'} 
+            >
+                <NavItem
+                    href={config.url.LOGIN}
+                    external
+                >
+                    Login
+                </NavItem>
+            </NavBar>
+    )
+}
