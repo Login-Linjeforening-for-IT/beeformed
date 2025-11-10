470ba883732734f6f7e752c6344761351dd65233
0herm <0herm@protonmail.com>
2025-11-10 15:13:12 +0100

    fix user

--- api/src/handlers/users/post.ts
+++ api/src/handlers/users/post.ts
@@ -1,19 +1,22 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-
 import { createEntity } from '../../utils/crud.ts'
 
+type CreateUserBody = {
+    email: string
+    name: string
+}
+
 export default async function createUser(req: FastifyRequest, res: FastifyReply) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const body = req.body as any
+    const body = req.body as CreateUserBody
     await createEntity({
         req,
         res,
         sqlPath: 'users/post.sql',
-        requiredFields: ['user_id', 'email'],
+        requiredFields: ['user_id', 'email', 'name'],
         sqlParams: [
-            body.user_id,
+            req.user!.id,
             body.email,
-            body.name || null
+            body.name
         ]
     })
 }
--- db/init.sql
+++ db/init.sql
@@ -2,7 +2,7 @@
 CREATE TABLE users (
     user_id TEXT PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
-    name TEXT,
+    name TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
